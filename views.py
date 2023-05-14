c5567800e6d305c2a1f0b307de677bc115e74fe2
sriramoji123 <46554135+sriramoji123@users.noreply.github.com>
2023-05-14 15:48:36 +0530

    Update views.py

--- views.py
+++ views.py
@@ -1,15 +1,14 @@
-Subject: Request for API Health Status
+import time
+class SessionUpdateMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
 
-Dear [Team],
+    def __call__(self, request):
+        if 'last_update_time' not in request.session:
+            request.session['last_update_time'] = time.time()
+        elif time.time() - request.session['last_update_time'] > 10:
+            request.session['last_update_time'] = time.time()
+            # update session value here
 
-I am writing this email to request information regarding the health status of the APIs that are being used in our Dataminer tool. As you know, the tool is heavily dependent on these APIs, and any downtime can severely impact the functionality of the tool. Therefore, I would like to request that you provide me with information regarding the health status of the APIs.
-
-It would be of great help if you could provide us with a dashboard or some sort of status page that can provide real-time updates on the health status of the APIs. This would allow us to take proactive measures to reduce the consumption of these APIs during their downtime.
-
-In addition, I would like to suggest that we implement a failover mechanism to handle situations where an API goes down unexpectedly. This will ensure that the tool continues to function without any interruptions, even if one or more APIs are down.
-
-I appreciate your help in this matter and look forward to hearing from you soon.
-
-Thank you,
-
-[Your Name]
+        response = self.get_response(request)
+        return response
